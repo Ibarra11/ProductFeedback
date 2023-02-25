69925c6236be66779d45f3946118d547081c8697
Alan Ibarra <alanjibarradev@gmail.com>
2023-02-24 21:41:21 -0800

    update the empty view when there are no post

--- app/components/FeedbackView/index.tsx
+++ app/components/FeedbackView/index.tsx
@@ -5,16 +5,36 @@ import clsx from "clsx";
 import data from "../../../data.json";
 import EmptySuggestionsView from "../EmptySuggestionsView";
 import ProductRequestList from "../ProductRequestList";
+import { useFilterContext } from "../FilterProvider";
+import { useSortContext } from "../SortProvider";
+import { sortPosts } from "@/app/utils";
 
 function FeedbackView() {
   const [posts, setPosts] = React.useState<Post[]>(
     data.productRequests as any as Post[]
   );
 
+  const { filterCategory } = useFilterContext();
+  const { sortBy } = useSortContext();
+
+  const filteredPost =
+    filterCategory === "All"
+      ? posts
+      : posts.filter(
+          (product) =>
+            product.category.toLowerCase() === filterCategory.toLowerCase()
+        );
+
+  const displayedPosts = sortPosts(filteredPost, sortBy);
+
   return (
     <div className={clsx(" flex-1 px-6 pb-14", " md:p-0 md:h-full  ")}>
       <div className="h-full w-full">
-        {posts.length > 0 ? <ProductRequestList /> : <EmptySuggestionsView />}
+        {displayedPosts.length > 0 ? (
+          <ProductRequestList posts={posts} />
+        ) : (
+          <EmptySuggestionsView />
+        )}
       </div>
     </div>
   );
--- app/components/ProductRequestList/index.tsx
+++ app/components/ProductRequestList/index.tsx
@@ -3,33 +3,14 @@ import React from "react";
 import clsx from "clsx";
 import ProductRequestPost from "../ProductRequestPost";
 import data from "../../../data.json";
-import { useFilterContext } from "../FilterProvider";
-import { useSortContext } from "../SortProvider";
 import { Post } from "@/types";
-import { sortPosts } from "@/app/utils";
 
 const POSTS = data.productRequests;
 
-function ProductRequestList() {
-  const [productRequest, setProductRequest] = React.useState<Post[]>(
-    POSTS as any
-  );
-  const { filterCategory } = useFilterContext();
-  const { sortBy } = useSortContext();
-
-  const filteredPost =
-    filterCategory === "All"
-      ? productRequest
-      : productRequest.filter(
-          (product) =>
-            product.category.toLowerCase() === filterCategory.toLowerCase()
-        );
-
-  const displayedPosts = sortPosts(filteredPost, sortBy);
-
+function ProductRequestList({ posts }: { posts: Post[] }) {
   return (
     <div className={clsx("flex flex-col gap-4", "lg:gap-5")}>
-      {displayedPosts.map((post) => (
+      {posts.map((post) => (
         <ProductRequestPost key={post.id} {...post} />
       ))}
     </div>
